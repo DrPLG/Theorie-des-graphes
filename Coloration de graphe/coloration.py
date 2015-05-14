b1adc8c843aecabc3944c4638d2dbf48a11a91f0
DrPLG <dr>
2015-05-14 20:20:34 +0200

    # V 0.05 Lecture de fichiers
    # V 0.05 Lecture de fichiers pour tester sur le programme sur d'autres
    graphes.

--- Coloration de graphe/coloration.py
+++ Coloration de graphe/coloration.py
@@ -5,18 +5,47 @@
 # V 0.01 Mise en place d'un graphe, et coloration de chaque sommet par une couleur distincte. Validé le 13.05.2015
 # V 0.02 Mise en place de l'algorithme glouton. Validé le 13.05.2015
 # V 0.03 Mise en place de l'algorithme DSATUR. Validé le 14.05.2015
-
-
+# V 0.04 Calcul de la taille de la clique maximale, pour obtenir un minorant du nombre chromatique. Validé le 14.05.2015
+# V 0.05 Lecture de fichiers pour tester sur le programme sur d'autres graphes. Validé le 14.05.2015
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import sys
+
+
+
+# Emplacement du fichier data
+fileLocation = sys.argv[1].strip()
+inputDataFile = open(fileLocation, 'r')
+inputData = ''.join(inputDataFile.readlines())
+inputDataFile.close()
+
+# Lecture du fichier data
+lines = inputData.split('\n')
+tempData = open('tempData','w')
+firstLine = lines[0].split()
+nodeCount = int(firstLine[0])
+edgeCount = int(firstLine[1])
 
 
 # Création du graphe
-G = nx.Graph()
-H = range(8)
-G.add_nodes_from(H,dsat=0)
-G.add_edges_from([(1,2),(1,3),(4,5),(5,7),(4,7),(1,7),(3,6)])
+G=nx.Graph()
+G.add_nodes_from(range(0,nodeCount))
+
+for i in range(1, edgeCount+1):
+    line=lines[i]
+    parts = line.split()
+    G.add_edge(int(parts[0]), int(parts[1]))
+
+
+
+
+
+## Création du graphe
+#G = nx.Graph()
+#H = range(8)
+#G.add_nodes_from(H,dsat=0)
+#G.add_edges_from([(1,2),(1,3),(4,5),(5,7),(4,7),(1,7),(3,6)])
 
 # Ordre des cliques maximales
 minorant = nx.graph_clique_number(G)
@@ -96,6 +125,14 @@ else:
     print "Le nombre chromatique est compris entre "+str(minorant)+" et "+str(numberOfColors)+"."
               
 # Tracé du graphe
-pos=nx.spring_layout(G)
-nx.draw_networkx(G,pos,node_color=nx.get_node_attributes(G,'color').values())
+pos={0:(1,2),
+     1:(1,0),
+     2:(0,1),
+     3:(2,1),
+     4:(4,2),
+     5:(4,0),
+     6:(3,1),
+     7:(5,1)}
+pos=nx.shell_layout(G)
+#nx.draw_networkx(G,pos,node_color=nx.get_node_attributes(G,'color').values())
 plt.show()
